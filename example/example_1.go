3dcba5328c55b1ccab9c3509615d7e937f5eaae5
kyden <kydenul@gmail.com>
2025-06-24 12:43:32 +0800

    Refactor: Rename example file and update SQL query for demo

--- example/example_1.go
+++ example/example_1.go
@@ -9,9 +9,7 @@ import (
 
 func main() {
 	// 创建提取器
-	extractor := sqlextractor.NewExtractor(
-		"SELECT * FROM users WHERE age > 18 AND name LIKE 'John%';SELECT a FROM t1 WHERE a = 1 AND b = 2;",
-	)
+	extractor := sqlextractor.NewExtractor("select lId,sBizCode,iGoodsId,iNum,sExt from dbUserCart_8.tbUserCart_49 where sUid='9764196850946095192' and sGoodsApp='9866' and sBizCode='ty' and sGroupKey ='areaid=170&platid=0&partition=17&roleid=17298608743218413570'") //nolint:lll,golines
 
 	// 提取 SQL 信息
 	err := extractor.Extract()
@@ -28,6 +26,9 @@ func main() {
 
 	tis := extractor.TableInfos()
 	fmt.Printf("表信息: %v\n", tis) // 返回 [][]*models.TableInfo
+
+	fmt.Println(extractor.TemplatizedSQLHash())
+	fmt.Println(extractor.TemplatizedSQL())
 
 	for idx, ti := range tis {
 		fmt.Printf("第 %d 个SQL表信息\n", idx)
